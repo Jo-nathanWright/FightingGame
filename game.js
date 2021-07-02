f9898d991a2818c5443b09458e735ca77420248a
Jonathan Wright <j.wright72127@gmail.com>
2021-07-01 18:02:20 -0600

    Added some style and functionality

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log('Hello Out There!')
+let health = 100
+
+function punch() {
+    health = health - 1
+    console.log('Lil punch')
+}
+function kick() {
+    health = health - 2
+    console.log('Solid kick')
+}
+function headbutt() {
+    health = health - 5
+    console.log('Huge Headbutt')
+}
